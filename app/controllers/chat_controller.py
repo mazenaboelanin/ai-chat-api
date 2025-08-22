caa92c785b76ddf485d8ce17bc27cc1e29425ff4
Mazen Abo Elanin Ahmed <39408280+mazenaboelanin@users.noreply.github.com>
2025-08-23 00:45:44 +0300

    Feat: add chat actions - get_chat_history by user - get_all_chats for all users - get_answer from ai agent by user

--- app/controllers/chat_controller.py
+++ app/controllers/chat_controller.py
@@ -1,18 +1,119 @@
 from flask import request, jsonify
+from config.db import db
+from ..services.ai_agent_api_service import request_ai_agent
+from ..models.message import Message
+from ..models.user import User
+from sqlalchemy import desc
 
-from services.ai_api_service import request_ai_agent
 
-def create_chat():
-    message = "welcome"
+def get_chat_history(user_id):
+  if not user_id:
+    return jsonify({"error": "user_id is required"}), 400
+
+  user = db.session.get(User, user_id)
+  if not user:
+    return jsonify({"error": f"No user found with id {user_id}"}), 404
+
+  stmt = (
+    db.select(Message)
+    .where(Message.user_id == user_id)
+    .order_by(desc(Message.created_at))
+  )
+  messages = db.session.execute(stmt).scalars().all()
+
+  return jsonify({
+    "messages": [
+        {
+            "id": message.id,
+            "answer": message.answer,
+            "question": message.question,
+            "user_id": message.user_id,
+            "created_at": message.created_at.isoformat() if message.created_at else None
+        }
+        for message in messages
+      ]
+  }), 200
+
+
+def get_answer():
+  data = request.get_json()
+  print("########### data", data)
+
+  if not data or "model" not in data or "question" not in data:
+    return jsonify({"error": "Model and Question must be specified"}), 400
+  
+  if "user_id" not in data:
+    return jsonify({"error": "user_id is required"}), 400
+
+  user_id = data["user_id"]
+
+  user = db.session.get(User, user_id)
+  if not user:
+    return jsonify({"error": f"No user found with id {user_id}"}), 404
+
+
+  mappedData = data.copy()
+  mappedData["stream"] = False
+
+  if "question" in mappedData:
+    mappedData["prompt"] = mappedData.pop("question")
+
+  if "user_id" in mappedData:
+    mappedData.pop("user_id")
+
+  try:
+    response = request_ai_agent(mappedData)
+
+    if response:
+      print("########### response", response)
+      print("######## response type:", type(response))
+      print("######## response value:", response)
+      new_message = Message(
+        question=data["question"],
+        answer=response,
+        user_id=user.id, 
+      )
+      db.session.add(new_message)
+      db.session.commit()
+    
+    return jsonify({
+      "success": True,
+      "message": "Answer returned successfully",
+      "answer": response
+    }), 200
+
+  except Exception as e:
+    return jsonify({
+      "success": False,
+      "message": "Failed to get answer",
+      "error": str(e)
+    }), 500
+
+def get_all_chats():
+  try:
+    stmt = (
+      db.select(Message)
+      .order_by(desc(Message.created_at))
+    )
+    messages = db.session.execute(stmt).scalars().all()
 
     return jsonify({
-        "status": "success",
-        "message": f"Chat message saved: {message}"
-    })
+      "messages": [
+        {
+          "id": message.id,
+          "answer": message.answer,
+          "question": message.question,
+          "user_id": message.user_id,
+          "created_at": message.created_at.isoformat() if message.created_at else None
+        }
+        for message in messages
+      ]
+    }), 200
 
-def get_chat_history():
-    history = [
-        {"id": 1, "message": "Hello"},
-        {"id": 2, "message": "How are you?"}
-    ]
-    return jsonify({"chat_history": history})
+  except Exception as e:
+    return jsonify({
+      "success": False,
+      "message": "Failed to get chats",
+      "error": str(e)
+    }), 404
+
--- app/routes/chat_routes.py
+++ app/routes/chat_routes.py
@@ -1,15 +1,19 @@
 from flask import Blueprint
-from controllers.chat_controller import create_chat, get_chat_history
+from ..controllers.chat_controller import get_answer, get_chat_history, get_all_chats
 
-chat_bp = Blueprint("chat", __name__, url_prefix="/chat")
+chat_bp = Blueprint("chat", __name__, url_prefix="/api/v1/chat")
 
-# POST /chat → create new message
-@chat_bp.route("/", methods=["GET"])
-def chat_post():
-  return create_chat()
+# POST /chat/ask → ask question
+@chat_bp.route("/ask", methods=["POST"])
+def ask_question():
+  return get_answer()
 
-# GET /chat/history → fetch chat history
-@chat_bp.route("/history", methods=["GET"])
-def chat_history():
-  return get_chat_history()
+# GET /chat/history/<user_id> → fetch chat history
+@chat_bp.route("/history/<user_id>", methods=["GET"])
+def chat_history(user_id):
+  return get_chat_history(user_id)
 
+# POST /chat/all → fetch all messages with all users
+@chat_bp.route("/all", methods=["GET"])
+def fetch_all_chats():
+  return get_all_chats()
